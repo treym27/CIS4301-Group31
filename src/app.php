d7ede2c69ae9ba9d12dba1a0e00c120626b20445
Mark Turiansky <mark.turiansky@yahoo.com>
2017-03-06 23:55:36 -0500

    adding providers

--- src/app.php
+++ src/app.php
@@ -9,6 +9,17 @@ use Silex\Provider\DoctrineServiceProvider;
 $app = new Application();
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
+$app->register(new SessionServiceProvider());
+$app->register(new DoctrineServiceProvider(), array(
+    "db.options" => array (
+        "driver"    =>  "oci8",
+        "user"      =>  "",
+        "password"  =>  "",
+        "host"      =>  "",
+        "port"      =>  0,
+        "dbname"    =>  ""
+    )
+));
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
