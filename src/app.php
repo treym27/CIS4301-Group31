1f8903e7068151253ec11add54efe7e9c51ff4c4
Mark Turiansky <mark.turiansky@yahoo.com>
2017-03-06 23:29:23 -0500

    got rid of some things

--- src/app.php
+++ src/app.php
@@ -7,10 +7,8 @@ use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 
 $app = new Application();
-$app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
-$app->register(new HttpFragmentServiceProvider());
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
