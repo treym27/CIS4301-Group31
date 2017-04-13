3e36dae2d45c7c4a0ba6f29bec2d92c9c9aed652
treym27 <treym27@ufl.edu>
2017-04-12 21:32:04 -0400

    Update controllers.php

--- src/controllers.php
+++ src/controllers.php
@@ -52,7 +52,7 @@ $app->get('/user/{email}', function ($email) use ($app) {
 })->bind('user_profile');
 
 // PAGE 2 EDIT USER INFO (TREY)
-$app->get('/user/timeline', function () use ($app) {
+$app->get('/user/acc_info', function () use ($app) {
     return $app['twig']->render('index.html.twig');
 })->bind('acc_info');
 
